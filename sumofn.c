48952878000694792c223a8169b3ae8d1bfc620b
darshanshah262 <48462723+darshanshah262@users.noreply.github.com>
2019-05-06 09:29:52 +0530

    Update sumofn.c

--- sumofn.c
+++ sumofn.c
@@ -1,37 +1,37 @@
 #include<stdio.h>
- int input(int n);
- int compute(int a[],int n);
- void output(int sum);
- int main()
- {
-         int a[100],n,s;
-         printf("Enter the number of elemnts\n");
-         scanf("%d",&n);
-         a[]=input(n);
-         s=compute(a,n);
-         output(s);
- }
- int input(int n)
- {
-         int i,a[100];
-         printf("Enter the nuber of elementsn");
-         for(i=0;i<n;i++)
-         {
-                 scanf("%d",&a[i]);
-         }
-         return a[];
- )
- int compute(int a[],int n)
- {
-         int i,sum=0;
-         for(i=0;i<n;i++)
-         {
-                 sum=sum+a[i];
-         }
-         return sum;
- }
- void output(int sum)
- {
-         printf("The sum is:%d\n",sum);
- }
+int input(int n);
+int compute(int a[],int n);
+void output(int sum);
+int main()
+{
+        int a[100],n,s;
+        printf("Enter the number of elemnts\n");
+        scanf("%d",&n);
+        a[100]=input(n);
+        s=compute(a,n);
+        output(s);
+}
+int input(int n)
+{
+        int i,a[100];
+        printf("Enter the nuber of elementsn");
+        for(i=0;i<n;i++)
+        {
+                scanf("%d",&a[i]);
+                return a[i];
+        }
 
+}
+int compute(int a[],int n)
+{
+        int i,sum=0;
+        for(i=0;i<n;i++)
+        {
+                sum=sum+a[i];
+        }
+        return sum;
+}
+void output(int sum)
+{
+        printf("The sum is:%d\n",sum);
+}
