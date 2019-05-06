2907e894b3877c34b0f21b034a5fe9a7c1b4a6eb
darshanshah262 <48462723+darshanshah262@users.noreply.github.com>
2019-05-06 15:11:50 +0530

    Update addpbr.c

--- addpbr.c
+++ addpbr.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
-int getdata(int a)
+void getdata(int *a,int *b)
 {
-    printf("enter a value\n");
-    scanf("%d",&a);
-    return a;
+        printf("Enter the  two value \n");
+        scanf("%d%d",a,b);
 }
-
 void add(int a,int b, int *c)
 {
     *c=a+b;
 }
 void main()
 {
-    int a,b,x,*c;
-    a=getdata(x);
-    b=getdata(x);
+    int a,b,c;
+    getdata(&a,&b);
     add(a,b,&c);
-     printf("%d\n",a);
-     printf("%d\n",b);
+     printf("1st no:%d\n",a);
+     printf("2nd no:%d\n",b);
      printf(" the sum is %d\n",c);
 }
+
+
+
