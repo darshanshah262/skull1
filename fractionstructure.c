841f1d10505e5c68fe1281c76b9292d632e4dabc
darshanshah262 <48462723+darshanshah262@users.noreply.github.com>
2019-05-07 14:35:21 +0530

    Update fractionstructure.c

--- fractionstructure.c
+++ fractionstructure.c
@@ -3,23 +3,29 @@ struct frac
 {
         int num;
         int den;
-}s1,s2;
- input()
+};
+ struct frac input()
 {
+        struct frac s1;
         printf("Enter the nume and deno \n");
         scanf("%d%d",&s1.num,&s1.den);
         return s1;
 }
- get()
+struct frac get()
 {
+        struct frac s2;
         printf("Enter the num and deno for second fraction\n");
         scanf("%d%d",&s2.num,&s2.den);
         return s2;
 }
 int main()
 {
-        int a,b;
-        a=input();
-        b=get();
-        printf("%d\n%d",a,b);
+
+        struct frac s,s1;
+        s=input();
+        printf("%d/%d\n",s.num,s.den);
+        s1=get();
+        printf("%d/%d\n",s1.num,s1.den);
+
+
 }
