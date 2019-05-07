739f635d75882a43cfc3caf382b420e070241b65
darshanshah262 <48462723+darshanshah262@users.noreply.github.com>
2019-05-07 11:20:42 +0530

    Update sumoffractions.c

--- sumoffractions.c
+++ sumoffractions.c
@@ -1,32 +1,38 @@
-
 #include<stdio.h>
-int getdata()
+int getdatan()
 {
-        int a,b;
-        float f;
+        int a;
         printf("Enter the numerator\n");
         scanf("%d",&a);
+        return a;
+}
+int getdaatad()
+{
+        int b;
         printf("Enter the denominator\n");
         scanf("%d",&b);
-        f=(float)a/b;
-        return f;
+        return b;
 }
-int compute(float a,float b)
+int compute(int a,int c,int b,int d,int *x3,int *x4)
 {
-        float c;
-        c=a+b;
-        return c;
+        //int x3,x4;
+        x3=(a*d)+(b*c);
+        x4=b+d;
 }
-void output(float a)
+void output(int a,int b)
 {
-        printf("The sum of two fractions is: %f\n");
+        printf("The sum of two fractions is: %d/%d\n",a,b);
 }
 int main()
 {
-        float a,b,ans;
-        a=(float)getdata();
-        b=getdata();
-        ans=compute(a,b);
-        output(ans);
+        int x1,y1,x2,y2,x3,x4,ans;
+        x1=getdatan();
+        y1=getdatad();
+        printf("The first fraction is: %d/%d",x1,y1);
+        x2=getdatan();
+        y2=getdatad();
+        printf("The second fraction is: %d/%d",x2,y2);
+        compute(x1,x2,y1,y2,&x3,&x4);
+        output(x3,x4);
         return 0;
 }
